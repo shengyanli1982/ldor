6b9f57d9d35a766f32b9b05de5f855890e5e1f0b
shengyanli1982 <shengyanlee36@gmail.com>
2024-09-10 18:38:58 +0800

    git cache cleaning

---
+++ go.mod
@@ -0,0 +1,70 @@
+module github.com/shengyanli1982/ldor
+
+go 1.21
+
+require (
+	github.com/gin-gonic/gin v1.9.1
+	github.com/shengyanli1982/gs v0.1.5
+	github.com/shengyanli1982/law v0.1.16
+	github.com/shengyanli1982/orbit v0.1.8
+	github.com/shengyanli1982/toolkit/pkg/command v0.0.0-20240829143620-c99268455b35
+	github.com/spf13/cobra v1.8.1
+	github.com/tidwall/gjson v1.17.3
+	github.com/tidwall/sjson v1.2.5
+	go.uber.org/zap v1.27.0
+	golang.org/x/net v0.26.0
+)
+
+require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/bytedance/sonic v1.11.9 // indirect
+	github.com/bytedance/sonic/loader v0.1.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cloudwego/base64x v0.1.4 // indirect
+	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/gabriel-vasile/mimetype v1.4.4 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/zapr v1.3.0 // indirect
+	github.com/go-openapi/jsonpointer v0.21.0 // indirect
+	github.com/go-openapi/jsonreference v0.21.0 // indirect
+	github.com/go-openapi/spec v0.21.0 // indirect
+	github.com/go-openapi/swag v0.23.0 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.22.0 // indirect
+	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.8 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/prometheus/client_golang v1.19.1 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.48.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/swaggo/files v1.0.1 // indirect
+	github.com/swaggo/gin-swagger v1.6.0 // indirect
+	github.com/swaggo/swag v1.16.3 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.12 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/arch v0.8.0 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/tools v0.22.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ internal/proxy.go
@@ -0,0 +1,375 @@
+package internal
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tidwall/gjson"
+	"github.com/tidwall/sjson"
+	"go.uber.org/zap"
+	"golang.org/x/net/http2"
+)
+
+const (
+	StableCodeModelPrefix = "stable-code"
+	DeepSeekCoderModel    = "deepseek-coder"
+)
+
+var (
+	ErrorConfigureTransport = errors.New("config transport failed")
+)
+
+type Pong struct {
+	Now    int    `json:"now"`
+	Status string `json:"status"`
+	Ns1    string `json:"ns1"`
+}
+
+type ProxyService struct {
+	log    *zap.SugaredLogger
+	cfg    *ServiceConfig
+	client *http.Client
+}
+
+func NewProxyService(cfg *ServiceConfig, log *zap.SugaredLogger) (*ProxyService, error) {
+	client, err := getClient(cfg)
+	if nil != err {
+		return nil, err
+	}
+
+	return &ProxyService{
+		log:    log,
+		cfg:    cfg,
+		client: client,
+	}, nil
+}
+
+func (ps *ProxyService) RegisterGroup(g *gin.RouterGroup) {
+	g.GET("/_ping", ps.pong)
+	g.GET("/models", ps.models)
+	g.GET("/v1/models", ps.models)
+	authToken := ps.cfg.AuthToken // replace with your dynamic value as needed
+	if authToken != "" {
+		// 鉴权
+		v1 := g.Group("/:token/v1/", AuthMiddleware(authToken))
+		{
+			v1.POST("/chat/completions", ps.chatCompletions)
+			v1.POST("/engines/copilot-codex/completions", ps.codeCompletions)
+			v1.POST("/v1/chat/completions", ps.chatCompletions)
+			v1.POST("/v1/engines/copilot-codex/completions", ps.codeCompletions)
+		}
+	} else {
+		g.POST("/v1/chat/completions", ps.chatCompletions)
+		g.POST("/v1/engines/copilot-codex/completions", ps.codeCompletions)
+		g.POST("/v1/v1/chat/completions", ps.chatCompletions)
+		g.POST("/v1/v1/engines/copilot-codex/completions", ps.codeCompletions)
+	}
+}
+
+func (ps *ProxyService) pong(c *gin.Context) {
+	c.JSON(http.StatusOK, Pong{
+		Now:    time.Now().Second(),
+		Status: "ok",
+		Ns1:    "200 OK",
+	})
+}
+
+func (s *ProxyService) models(c *gin.Context) {
+	c.JSON(http.StatusOK, defaultModels)
+}
+
+func (s *ProxyService) codeCompletions(c *gin.Context) {
+	ctx := c.Request.Context()
+	if ctx.Err() != nil {
+		abortCodex(c, http.StatusRequestTimeout)
+		return
+	}
+
+	body, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		s.log.Errorf("Failed to read request body: %v", err)
+		abortCodex(c, http.StatusBadRequest)
+		return
+	}
+
+	body = ConstructRequestBody(body, s.cfg, s.log)
+
+	proxyURL := s.cfg.CodexApiBase + "/completions"
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, proxyURL, bytes.NewReader(body))
+	if err != nil {
+		s.log.Errorf("Failed to create request: %v", err)
+		abortCodex(c, http.StatusInternalServerError)
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+s.cfg.CodexApiKey)
+	if s.cfg.CodexApiOrganization != "" {
+		req.Header.Set("OpenAI-Organization", s.cfg.CodexApiOrganization)
+	}
+	if s.cfg.CodexApiProject != "" {
+		req.Header.Set("OpenAI-Project", s.cfg.CodexApiProject)
+	}
+
+	resp, err := s.client.Do(req)
+	if err != nil {
+		if errors.Is(err, context.Canceled) {
+			abortCodex(c, http.StatusRequestTimeout)
+		} else {
+			s.log.Errorf("Request completions failed: %v", err)
+			abortCodex(c, http.StatusInternalServerError)
+		}
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		s.log.Errorf("Request completions failed with status code %d: %s", resp.StatusCode, string(body))
+		abortCodex(c, resp.StatusCode)
+		return
+	}
+
+	c.Status(resp.StatusCode)
+	if contentType := resp.Header.Get("Content-Type"); contentType != "" {
+		c.Header("Content-Type", contentType)
+	}
+
+	_, err = io.Copy(c.Writer, resp.Body)
+	if err != nil {
+		s.log.Errorf("Failed to copy response body: %v", err)
+	}
+}
+
+func (s *ProxyService) chatCompletions(c *gin.Context) {
+	ctx := c.Request.Context()
+	if ctx.Err() != nil {
+		abortChat(c, http.StatusRequestTimeout)
+		return
+	}
+
+	body, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		s.log.Errorf("Failed to read request body: %v", err)
+		abortChat(c, http.StatusBadRequest)
+		return
+	}
+
+	body = s.constructChatRequestBody(body)
+
+	proxyURL := s.cfg.ChatApiBase + "/chat/completions"
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, proxyURL, bytes.NewReader(body))
+	if err != nil {
+		s.log.Errorf("Failed to create request: %v", err)
+		abortChat(c, http.StatusInternalServerError)
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+s.cfg.ChatApiKey)
+	if s.cfg.ChatApiOrganization != "" {
+		req.Header.Set("OpenAI-Organization", s.cfg.ChatApiOrganization)
+	}
+	if s.cfg.ChatApiProject != "" {
+		req.Header.Set("OpenAI-Project", s.cfg.ChatApiProject)
+	}
+
+	resp, err := s.client.Do(req)
+	if err != nil {
+		if errors.Is(err, context.Canceled) {
+			abortChat(c, http.StatusRequestTimeout)
+		} else {
+			s.log.Errorf("Request chat completions failed: %v", err)
+			abortChat(c, http.StatusInternalServerError)
+		}
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		s.log.Errorf("Request chat completions failed with status code %d: %s", resp.StatusCode, string(body))
+		abortChat(c, resp.StatusCode)
+		return
+	}
+
+	c.Status(resp.StatusCode)
+	if contentType := resp.Header.Get("Content-Type"); contentType != "" {
+		c.Header("Content-Type", contentType)
+	}
+
+	_, err = io.Copy(c.Writer, resp.Body)
+	if err != nil {
+		s.log.Errorf("Failed to copy response body: %v", err)
+	}
+}
+
+func (s *ProxyService) constructChatRequestBody(body []byte) []byte {
+	model := gjson.GetBytes(body, "model").String()
+	if mapped, ok := s.cfg.ChatModelMap[model]; ok {
+		model = mapped
+	} else {
+		model = s.cfg.ChatModelDefault
+	}
+	body, _ = sjson.SetBytes(body, "model", model)
+
+	if !gjson.GetBytes(body, "function_call").Exists() {
+		messages := gjson.GetBytes(body, "messages").Array()
+		lastIndex := len(messages) - 1
+		if !strings.Contains(messages[lastIndex].Get("content").String(), "Respond in the following locale") {
+			locale := s.cfg.ChatLocale
+			if locale == "" {
+				locale = "zh_CN"
+			}
+			body, _ = sjson.SetBytes(body, fmt.Sprintf("messages.%d.content", lastIndex), messages[lastIndex].Get("content").String()+"Respond in the following locale: "+locale+".")
+		}
+	}
+
+	body, _ = sjson.DeleteBytes(body, "intent")
+	body, _ = sjson.DeleteBytes(body, "intent_threshold")
+	body, _ = sjson.DeleteBytes(body, "intent_content")
+
+	maxTokens := gjson.GetBytes(body, "max_tokens").Int()
+	if int(maxTokens) > s.cfg.ChatMaxTokens {
+		body, _ = sjson.SetBytes(body, "max_tokens", s.cfg.ChatMaxTokens)
+	}
+
+	return body
+}
+
+func AuthMiddleware(authToken string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.Param("token")
+		if token != authToken {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
+
+func ConstructRequestBody(body []byte, cfg *ServiceConfig, log *zap.SugaredLogger) []byte {
+	var err error
+	body, err = sjson.DeleteBytes(body, "extra")
+	if err != nil {
+		// 处理错误，例如记录日志
+		log.Errorf("Error deleting fields: %v", err)
+	}
+
+	body, err = sjson.DeleteBytes(body, "nwo")
+	if err != nil {
+		// 处理错误，例如记录日志
+		log.Errorf("Error deleting fields: %v", err)
+	}
+
+	body, err = sjson.SetBytes(body, "model", cfg.CodeInstructModel)
+	if err != nil {
+		log.Errorf("Error setting model: %v", err)
+	}
+
+	maxTokens := gjson.GetBytes(body, "max_tokens").Int()
+	if int(maxTokens) > cfg.CodexMaxTokens {
+		body, err = sjson.SetBytes(body, "max_tokens", cfg.CodexMaxTokens)
+		if err != nil {
+			log.Errorf("Error setting max_tokens: %v", err)
+		}
+	}
+
+	switch {
+	case strings.Contains(cfg.CodeInstructModel, StableCodeModelPrefix):
+		return constructWithStableCodeModel(body)
+	case strings.HasPrefix(cfg.CodeInstructModel, DeepSeekCoderModel):
+		if gjson.GetBytes(body, "n").Int() > 1 {
+			body, err = sjson.SetBytes(body, "n", 1)
+			if err != nil {
+				log.Errorf("Error setting n: %v", err)
+			}
+		}
+		// TODO: 需要实现
+		// case strings.HasSuffix(cfg.ChatApiBase, "chat"):
+		// 	return constructWithChatModel(body, nil) // 需要实现
+	}
+
+	return body
+}
+
+func constructWithStableCodeModel(body []byte) []byte {
+	suffix := gjson.GetBytes(body, "suffix").String()
+	prompt := gjson.GetBytes(body, "prompt").String()
+	content := fmt.Sprintf("<fim_prefix>%s<fim_suffix>%s<fim_middle>", prompt, suffix)
+
+	messages := []map[string]string{
+		{
+			"role":    "user",
+			"content": content,
+		},
+	}
+	return constructWithChatModel(body, messages)
+}
+
+func constructWithChatModel(body []byte, messages interface{}) []byte {
+	var err error
+	body, err = sjson.SetBytes(body, "messages", messages)
+	if err != nil {
+		log.Printf("Error setting messages: %v", err)
+	}
+
+	jsonStr := string(body)
+	jsonStr = strings.NewReplacer(
+		"\\u003c", "<",
+		"\\u003e", ">",
+	).Replace(jsonStr)
+	return []byte(jsonStr)
+}
+
+func getClient(cfg *ServiceConfig) (*http.Client, error) {
+	transport := &http.Transport{
+		ForceAttemptHTTP2:   true,
+		DisableKeepAlives:   false,
+		MaxIdleConns:        100,
+		MaxIdleConnsPerHost: 100,
+		IdleConnTimeout:     90 * time.Second,
+	}
+
+	if err := http2.ConfigureTransport(transport); err != nil {
+		return nil, ErrorConfigureTransport
+	}
+
+	if cfg.ProxyUrl != "" {
+		proxyURL, err := url.Parse(cfg.ProxyUrl)
+		if err != nil {
+			return nil, err
+		}
+		transport.Proxy = http.ProxyURL(proxyURL)
+	}
+
+	client := &http.Client{
+		Transport: transport,
+		Timeout:   time.Duration(cfg.Timeout) * time.Second,
+	}
+
+	return client, nil
+}
+
+func abortCodex(c *gin.Context, status int) {
+	c.Header("Content-Type", "text/event-stream")
+
+	c.String(status, "data: [DONE]\n")
+	c.Abort()
+}
+
+func abortChat(c *gin.Context, status int) {
+	c.Header("Content-Type", "application/json")
+	c.JSON(status, gin.H{"error": "Chat completion failed"})
+	c.Abort()
+}
